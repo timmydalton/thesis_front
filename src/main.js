3c694b0d836d2ca9c297e4b9d87089cf3601b11b
KhanhDuyTr <timmydalton2k@gmail.com>
2024-05-20 09:21:56 +0700

    [inititation]

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import 'ant-design-vue/dist/reset.css'
+import '../style/main.sass'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
